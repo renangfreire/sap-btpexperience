158505f1ae687cf63f4c476d43f8aa8533e2b866
Renan Freire <renangomesfreire@gmail.com>
2023-12-05 13:28:00 -0300

    feat: in the pages XML and fragments creation process

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,13 +1,37 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("com.lab2dev.finalprojectprodev.controller.BaseController", {
-          openDialog(sPath){}
-      });
+  [
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/core/Fragment'
+  ],
+  function (BaseController, Fragment) {
+    "use strict";
+
+    return BaseController.extend("com.lab2dev.finalprojectprodev.controller.BaseController", {
+      DialogTypes: ["DeleteUser", "ImportUser"],
+      onOpenImportDialog(oEvent) {
+        const sDialog = this.DialogTypes.find(el => oEvent.getSource().getId().includes(el))
+
+        debugger
+
+        if (!this[sDialog]) {
+          this[sDialog] = Fragment.load({
+            name: `com.lab2dev.finalprojectprodev.view.fragments.${sDialog}Dialog`,
+            controller: this
+          })
+        }
+
+        this[sDialog].then(oDialog => {
+          this.getView().insertDependent(oDialog)
+          oDialog.open()
+        })
+      },
+      oncloseDialog: function(oEvent){
+        const sDialog = this.DialogTypes.find(el => oEvent.getSource().getId().includes(el))
+
+        this[sDialog].then((oDialog) => {
+            oDialog.close()
+        })
     }
-  );
-  
+    });
+  }
+);
--- webapp/controller/RegisterNewUsers.controller.js
+++ webapp/controller/RegisterNewUsers.controller.js
@@ -1,51 +1,19 @@
 sap.ui.define([
     "com/lab2dev/finalprojectprodev/controller/BaseController",
     "sap/ui/model/json/JSONModel",
-    'sap/ui/core/Fragment'
+    'sap/ui/core/Fragment',
 ],
-    function (Controller, JSONModel, Fragment) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.finalprojectprodev.controller.RegisterNewUsers", {
             onInit: function () {
-                const testData = [
-                    {id: 1, name: "Roger"},
-                    {id: 2, name: "Vinicius"},
-                    {id: 3, name: "Davi"},
-                    {id: 1, name: "Roger"},
-                    {id: 2, name: "Vinicius"},
-                    {id: 3, name: "Davi"},
-                    {id: 1, name: "Roger"},
-                    {id: 2, name: "Vinicius"},
-                    {id: 3, name: "Davi"},
-                    {id: 1, name: "Roger"},
-                    {id: 2, name: "Vinicius"},
-                    {id: 3, name: "Davi"},
-                ]  
 
-                const oModel = new JSONModel({Users: testData})
+                const oModel = new JSONModel()
+                oModel.loadData('/model/users.json')
 
                 this.getView().setModel(oModel)
             },
             // Sei que posso jogar essas linhas na BASE, porém tenho que ver com relação ao THIS.DIALOG
-            onOpenImportDialog: function() {
-                if(!this.dialog){
-                    this.dialog = Fragment.load({
-                        name: "com.lab2dev.finalprojectprodev.view.fragments.ImportUserDialog",
-                        controller: this
-                    })
-                }
-
-                this.dialog.then(oDialog => {
-                    this.getView().insertDependent(oDialog)
-                    oDialog.open()
-                })
-
-            },
-            oncloseDialog: function(){
-                this.dialog.then((oDialog) => {
-                    oDialog.close()
-                })
-            }
         });
     });
